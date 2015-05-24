f88f88e02761ca5386d145ff0097a34687442a9c
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-23 19:30:52 -0500

    Add `-o` option to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 
+use std::env;
+extern crate getopts;
+
 #[allow(dead_code)] // This shouldn't be necessary, but otherwise `cargo test` complains.
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = getopts::Options::new();
+    opts.optopt("o", "", "output binary location", "a.out");
+
+    let matches = opts.parse(&args[1..]).unwrap();
+    let output_file = matches.opt_str("o").unwrap_or("a.out".to_string());
+    println!("{:?}", output_file);
 }
 
