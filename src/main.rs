a199cf6d6d595633689eb323125870bfde691256
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-23 19:42:53 -0500

    Simplify env::args call

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ extern crate getopts;
 
 #[allow(dead_code)] // This shouldn't be necessary, but otherwise `cargo test` complains.
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
     let mut opts = getopts::Options::new();
     opts.optopt("o", "", "output binary location", "a.out");
 
-    let matches = opts.parse(&args[1..]).unwrap();
+    let matches = opts.parse(env::args().skip(1)).unwrap();
     let output_file = matches.opt_str("o").unwrap_or("a.out".to_string());
     println!("{:?}", output_file);
 }
