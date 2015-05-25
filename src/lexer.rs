8fa893e4929622c11b4063d3bf8e7b2372329b5d
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-25 09:52:47 -0500

    lexer.rs: define Token and Operator enums

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,28 @@
 
+/**
+ * Each of the variants in this enum represents one kind of C token.
+ */
+pub enum Token {
+    Identifier(String),
+    Operator(Operator),
+    Comma,
+    Semicolon,
+    LParen, RParen,
+    LBrace, RBrace,
+}
 
+/**
+ * These have a direct correspondence to the C operators of the same name.
+ *
+ * TODO: Bitwise operators.
+ */
+pub enum Operator {
+    Add,        // +
+    Subtract,   // -
+    Multiply,   // *
+    Divide,     // /
+    And,        // &&
+    Or,         // ||
+    Assignment, // =
+}
 
