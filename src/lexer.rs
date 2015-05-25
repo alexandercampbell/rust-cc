507d43945641bd64536c6462134afaf40d8b0a90
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-25 10:16:45 -0500

    lexer.rs: document `lex()` function (not impl yet)

--- src/lexer.rs
+++ src/lexer.rs
@@ -44,7 +44,8 @@ pub enum Operator {
 /**
  * Number describes the possible **literals** that can occur as a result of lexing.
  *
- * Presently, we don't try to support literals like "10L" or "10.0f". TODO: Implement those.
+ * Presently, we don't try to support literals like "10L" or "10.0f".
+ * TODO: Implement those.
  */
 pub enum Number {
     Int(i64),
@@ -70,5 +71,18 @@ impl Operator {
             _ => return None,
         })
     }
+}
+
+/**
+ * Convert from a str to a vector of Tokens.
+ *
+ * For example, the string `", ( {"` would be transformed into the Vector of Tokens
+ * `vec![Comma, LParen, LBrace]`.
+ *
+ * The result of this function is just a sequence of Token without hierarchy. These Tokens should
+ * be parsed to build a walkable AST.
+ */
+pub fn lex(s: &str) -> Result<Vec<Token>, &'static str> {
+    Err("not yet implemented")
 }
 
