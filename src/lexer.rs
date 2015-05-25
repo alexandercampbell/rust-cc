d88467856a058829e4baac573421c05e769652eb
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-25 10:05:07 -0500

    Token: document each variant with content

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,8 +3,23 @@
  * Each of the variants in this enum represents one kind of C token.
  */
 pub enum Token {
+    /// String is a literal pulled directly from source, however, it will probably have some
+    /// special processing done for escape sequences.
+    String(String),
+
+    /// Number is a literal pulled directly from source.
+    Number(Number),
+
+    /// Identifier could be a function call or variable, or even type declaration.
     Identifier(String),
+
+    /// Operators have to be handled with correct precedence, but that's a problem for the parser.
+    /// The lexer is only concerned with defining which Operators exist.
     Operator(Operator),
+
+    /*
+     * The Tokens below this point should be self-explanatory :)
+     */
     Comma,
     Semicolon,
     LParen, RParen,
@@ -24,6 +39,16 @@ pub enum Operator {
     And,        // &&
     Or,         // ||
     Assignment, // =
+}
+
+/**
+ * Number describes the possible **literals** that can occur as a result of lexing.
+ *
+ * Presently, we don't try to support literals like "10L" or "10.0f". TODO: Implement those.
+ */
+pub enum Number {
+    Int(i64),
+    Float(f64),
 }
 
 impl Operator {
