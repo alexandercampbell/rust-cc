8caab9188cd8b063fc1a4ca0ee74755ac6ad6ce0
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-06-05 17:41:00 -0400

    lib.rs: add crate docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+/*!
+ * rust-cc
+ * =======
+ *
+ * rust-cc will be a C compiler written in Rust.
+ *
+ * rust-cc is a cross between learning project and proof of concept. Do not use this program for C
+ * development.
+ *
+ * maintainer
+ * ----------
+ *
+ * - [Alexander Campbell](mailto:alexanderhcampbell@gmail.com)
+ *
+ */
 #![feature(box_syntax)]
 
 pub mod ast;
